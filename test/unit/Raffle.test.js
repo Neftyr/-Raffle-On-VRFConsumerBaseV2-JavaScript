88923c16a9d83cb1e3363f7e6a735612387c1a70
Niferu <wargacki.patryq@gmail.com>
2022-12-30 19:53:04 +0100

    Some Tests Finished

---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,70 @@
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", async function () {
+          let raffle, raffleContract, vrfCoordinatorV2Mock, raffleEntranceFee, interval, player // , deployer
+
+          beforeEach(async () => {
+              // could also do with getNamedAccounts
+              accounts = await ethers.getSigners()
+              // deployer = accounts[0]
+              player = accounts[1]
+              // Deploys modules with the tags "mocks" and "raffle"
+              await deployments.fixture(["mocks", "raffle"])
+              // Returns a new connection to the VRFCoordinatorV2Mock contract
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+              // Returns a new connection to the Raffle contract
+              raffleContract = await ethers.getContract("Raffle")
+              // Returns a new instance of the Raffle contract connected to player
+              raffle = raffleContract.connect(player)
+              raffleEntranceFee = await raffle.getEntranceFee()
+              interval = await raffle.getInterval()
+          })
+
+          describe("constructor", function () {
+              it("initializes the raffle correctly", async () => {
+                  // Ideally, we'd separate these out so that only 1 assert per "it" block
+                  const raffleState = (await raffle.getRaffleState()).toString()
+                  // Comparisons for Raffle initialization:
+                  assert.equal(raffleState, "0")
+                  assert.equal(interval.toString(), networkConfig[network.config.chainId]["keepersUpdateInterval"])
+              })
+          })
+
+          describe("enterRaffle", function () {
+              it("reverts when you don't pay enough", async () => {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith("Raffle__SendMoreToEnterRaffle")
+              })
+              it("allows players to participate and record them in raffle", async function () {
+                  const startingPlayers = await raffle.getNumberOfPlayers()
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const getPlayers = await raffle.getNumberOfPlayers()
+                  assert.equal(startingPlayers.toString(), "0")
+                  assert.equal(getPlayers.toString(), "1")
+              })
+              it("emits event when enter", async function () {
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(raffle, "RaffleEnter")
+              })
+              it("doesn't allow entrance when raffle is in calculating state", async function () {
+                  /* To turn raffle into "calculating" state we need to make below bool's true: */
+                  // bool isOpen
+                  // bool timePassed
+                  // bool hasPlayers
+                  // bool hasBalance
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  // Checking "bool timePassed"
+                  // For a documentation of the methods below, go here: https://hardhat.org/hardhat-network/reference
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  // Creating 1 extra block
+                  // await network.provider.send("evm_mine", []) -> It is the same as below
+                  await network.provider.request({ method: "evm_mine", params: [] })
+                  // We pretend to be a ChainLink Keeper for a second
+                  // Changes the state to calculating for our comparison below
+                  await raffle.performUpkeep([])
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith("Raffle__RaffleNotOpen")
+              })
+          })
+      })
